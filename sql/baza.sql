058c62adb81dd4bfc9429792b3f291df6d443aac
damian2ba <damian2ba@gmail.com>
2018-10-25 09:19:49 +0200

    update bazy
    dodałem relacje i załączyłem ich wykres

--- sql/baza.sql
+++ sql/baza.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 25 Paź 2018, 08:20
+-- Czas generowania: 25 Paź 2018, 09:18
 -- Wersja serwera: 10.1.19-MariaDB
 -- Wersja PHP: 5.6.28
 
@@ -40,7 +40,7 @@ CREATE TABLE `daty_matur` (
 CREATE TABLE `grupy_matura_ustna` (
   `id` int(11) NOT NULL,
   `id_matury_ustnej` int(3) NOT NULL,
-  `klasa` varchar(3) NOT NULL,
+  `klasa` int(11) NOT NULL,
   `ilosc_osob` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -64,12 +64,13 @@ CREATE TABLE `klasy` (
 
 CREATE TABLE `matury_ustne` (
   `id` int(11) NOT NULL,
-  `id_daty_egzaminu` date NOT NULL,
+  `id_daty_egzaminu` int(11) NOT NULL,
   `godzina_rozpoczecia` time NOT NULL,
   `godzina_zakonczenia` time NOT NULL,
-  `sala` varchar(4) NOT NULL,
+  `sala` varchar(5) NOT NULL,
   `egzaminator_wewnetrzny` int(11) NOT NULL,
-  `egzaminator_zewnetrzny` int(11) NOT NULL
+  `egzaminator_zewnetrzny` int(11) NOT NULL,
+  `id_przedmiotu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -108,7 +109,7 @@ CREATE TABLE `nauczyciele_zewnetrzni` (
 
 CREATE TABLE `przedmioty` (
   `id` int(11) NOT NULL,
-  `przedmiot` varchar(20) NOT NULL
+  `nazwa` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -129,7 +130,7 @@ CREATE TABLE `sale` (
 --
 
 CREATE TABLE `szkoly` (
-  `szkola` varchar(255) NOT NULL
+  `nazwa` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -147,7 +148,9 @@ ALTER TABLE `daty_matur`
 -- Indexes for table `grupy_matura_ustna`
 --
 ALTER TABLE `grupy_matura_ustna`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_klasa_grupa` (`klasa`),
+  ADD KEY `fk_idMaturyUstnej_grupyMaturyUstnej` (`id_matury_ustnej`);
 
 --
 -- Indexes for table `klasy`
@@ -159,7 +162,12 @@ ALTER TABLE `klasy`
 -- Indexes for table `matury_ustne`
 --
 ALTER TABLE `matury_ustne`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_przedmiot_maturyUstne` (`id_przedmiotu`),
+  ADD KEY `fk_sala_maturyUstne` (`sala`),
+  ADD KEY `fk_nauczycielW_maturyUstne` (`egzaminator_wewnetrzny`),
+  ADD KEY `fk_nauczycielZ_maturyUstne` (`egzaminator_zewnetrzny`) USING BTREE,
+  ADD KEY `fk_data_maturyUstne` (`id_daty_egzaminu`);
 
 --
 -- Indexes for table `nauczyciele_wewnetrzni`
@@ -171,26 +179,28 @@ ALTER TABLE `nauczyciele_wewnetrzni`
 -- Indexes for table `nauczyciele_zewnetrzni`
 --
 ALTER TABLE `nauczyciele_zewnetrzni`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_szkola_nauczyciele_zewnetrzni` (`szkola`);
 
 --
 -- Indexes for table `przedmioty`
 --
 ALTER TABLE `przedmioty`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `przedmiot` (`przedmiot`);
+  ADD UNIQUE KEY `przedmiot` (`nazwa`);
 
 --
 -- Indexes for table `sale`
 --
 ALTER TABLE `sale`
+  ADD PRIMARY KEY (`sala`),
   ADD UNIQUE KEY `sala` (`sala`);
 
 --
 -- Indexes for table `szkoly`
 --
 ALTER TABLE `szkoly`
-  ADD PRIMARY KEY (`szkola`);
+  ADD PRIMARY KEY (`nazwa`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -226,6 +236,34 @@ ALTER TABLE `nauczyciele_zewnetrzni`
 --
 ALTER TABLE `przedmioty`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `grupy_matura_ustna`
+--
+ALTER TABLE `grupy_matura_ustna`
+  ADD CONSTRAINT `fk_idMaturyUstnej_grupyMaturyUstnej` FOREIGN KEY (`id_matury_ustnej`) REFERENCES `matury_ustne` (`id`),
+  ADD CONSTRAINT `fk_klasa_grupa` FOREIGN KEY (`klasa`) REFERENCES `klasy` (`id`);
+
+--
+-- Ograniczenia dla tabeli `matury_ustne`
+--
+ALTER TABLE `matury_ustne`
+  ADD CONSTRAINT `fk_data_maturyUstne` FOREIGN KEY (`id_daty_egzaminu`) REFERENCES `daty_matur` (`id`),
+  ADD CONSTRAINT `fk_nauczycielW_maturyUstne` FOREIGN KEY (`egzaminator_wewnetrzny`) REFERENCES `nauczyciele_wewnetrzni` (`id`),
+  ADD CONSTRAINT `fk_nauczycielZ_maturyUstne` FOREIGN KEY (`egzaminator_zewnetrzny`) REFERENCES `nauczyciele_zewnetrzni` (`id`),
+  ADD CONSTRAINT `fk_nauczycielZ_maturyUstne2` FOREIGN KEY (`egzaminator_zewnetrzny`) REFERENCES `nauczyciele_zewnetrzni` (`id`),
+  ADD CONSTRAINT `fk_przedmiot_maturyUstne` FOREIGN KEY (`id_przedmiotu`) REFERENCES `przedmioty` (`id`),
+  ADD CONSTRAINT `fk_sala_maturyUstne` FOREIGN KEY (`sala`) REFERENCES `sale` (`sala`);
+
+--
+-- Ograniczenia dla tabeli `nauczyciele_zewnetrzni`
+--
+ALTER TABLE `nauczyciele_zewnetrzni`
+  ADD CONSTRAINT `fk_szkola_nauczyciele_zewnetrzni` FOREIGN KEY (`szkola`) REFERENCES `szkoly` (`nazwa`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
