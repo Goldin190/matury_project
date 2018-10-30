3e82aba83f6b08c6eb3cd58ab36795a57029158c
Maciej Nowowiejski <44382787+Goldin190@users.noreply.github.com>
2018-10-30 11:21:50 +0100

    Add files via upload

--- web/front-end/scripts.js
+++ web/front-end/scripts.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
     $(".nav-link").click(function(){
-        $(".active").removeClass("active show");
+        $("a").removeClass("active show");
         $(this).addClass("show");
         $(this).addClass("active");
+        $("div.dropdown").setAttribute("aria-extended","true");
     });
+    $("button.dropdown-toggle.matury").click(function(){
+       $("div.matury").addClass("show");
+       $("div.matura-menu").addClass("show");
+        $("div.nauczyciele").removeClass("show");
+        $("div.nauczyciele-menu").removeClass("show");
+    });
+    $("button.dropdown-toggle.nauczyciele").click(function(){
+        $("div.nauczyciele").addClass("show");
+        $("div.nauczyciele-menu").addClass("show");
+        $("div.matury").removeClass("show");
+       $("div.matura-menu").removeClass("show");
+    })
 });
